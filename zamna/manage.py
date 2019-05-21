d34e8c91f083abfef43b34c015dda0df1e5549ae
Unknown <nis16104@uvg.edu.gt>
2019-05-21 14:49:04 -0600

    more configs

--- zamna/manage.py
+++ zamna/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zamna.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zamna.settings.base.py')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
